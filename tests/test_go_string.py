4aa87e6b342cdbd208cb40520e7de9f93b02ccea
Code-Apprentice <codeguru42@gmail.com>
2024-09-29 15:17:12 -0600

    Test for GoString.merge_with()

---
+++ tests/test_go_string.py
@@ -0,0 +1,14 @@
+from gobot.goboard_slow import GoString
+from gobot.gotypes import Player, Point
+
+
+def test_go_string_merge():
+    p1 = Point(4, 4)
+    p2 = Point(4, 5)
+    string1 = GoString(Player.BLACK, {p1}, p1.neighbors())
+    string2 = GoString(Player.BLACK, {p2}, p2.neighbors())
+    expected = GoString(
+        Player.BLACK, {p1, p2}, set(p1.neighbors()) | set(p1.neighbors()) - {p1, p2}
+    )
+    result = string1.merge_with(string2)
+    assert result == expected

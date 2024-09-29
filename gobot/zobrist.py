3e738707b5860aee8456e08318ae4bfb76361826
Code-Apprentice <codeguru42@gmail.com>
2024-09-29 16:26:50 -0600

    Zobrist hashes

---
+++ gobot/zobrist.py
@@ -0,0 +1,16 @@
+import random
+
+from gobot.gotypes import Player, Point
+
+MAX63 = 0x7FFFFFFFFFFFFFFF
+
+table = {}
+empty_board = 0
+for row in range(1, 20):
+    for col in range(1, 20):
+        for state in (Player.BLACK, Player.WHITE):
+            code = random.randint(0, MAX63)
+            table[Point(row, col), state] = code
+
+HASH_CODE = table
+EMPTY_BOARD = empty_board

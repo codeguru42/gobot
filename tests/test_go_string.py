c3297ca98e172cbc2b5ce809829603c2e0cf1690
Code-Apprentice <codeguru42@gmail.com>
2024-09-29 15:20:02 -0600

    Fix test

--- tests/test_go_string.py
+++ tests/test_go_string.py
@@ -8,7 +8,7 @@ def test_go_string_merge():
     string1 = GoString(Player.BLACK, {p1}, p1.neighbors())
     string2 = GoString(Player.BLACK, {p2}, p2.neighbors())
     expected = GoString(
-        Player.BLACK, {p1, p2}, set(p1.neighbors()) | set(p1.neighbors()) - {p1, p2}
+        Player.BLACK, {p1, p2}, set(p1.neighbors()) | set(p2.neighbors()) - {p1, p2}
     )
     result = string1.merge_with(string2)
     assert result == expected

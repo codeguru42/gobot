3b110a8791ad0627a33d2346fbe5dcb3cd0b6153
Code-Apprentice <codeguru42@gmail.com>
2024-09-29 13:37:03 -0600

    Detect eyes

---
+++ agents/helpers.py
@@ -0,0 +1,31 @@
+from gobot.goboard_slow import Board
+from gobot.gotypes import Point, Player
+
+
+def is_point_an_eye(board: Board, point: Point, color: Player) -> bool:
+    if board.get_go_string(point) is not None:
+        return False
+    for neighbor in point.neighbors():
+        if board.is_on_grid(neighbor):
+            neighbor_color = board.get(neighbor)
+            if neighbor_color != color:
+                return False
+
+    friendly_corners = 0
+    off_board_corners = 0
+    corners = [
+        Point(point.row - 1, point.col - 1),
+        Point(point.row - 1, point.col + 1),
+        Point(point.row + 1, point.col - 1),
+        Point(point.row + 1, point.col + 1),
+    ]
+    for corner in corners:
+        if board.is_on_grid(corner):
+            corner_color = board.get(corner)
+            if corner_color == color:
+                friendly_corners += 1
+        else:
+            off_board_corners += 1
+    if off_board_corners > 0:
+        return off_board_corners + friendly_corners == 4
+    return friendly_corners >= 3

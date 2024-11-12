8dff104551df791c28909d1425c4475bfb5e30c7
Code-Apprentice <codeguru42@gmail.com>
2024-11-12 08:33:07 -0700

    Move test files to subfolder

--- tests/board/test_legal_moves.py
+++ tests/board/test_legal_moves.py
@@ -8,7 +8,7 @@ from go.gotypes import Player, Point
 
 @pytest.mark.parametrize(
     "filename,num_rows,num_cols,next_player,point",
-    [(Path(__file__).parent / "board4.txt", 9, 9, Player.BLACK, Point(1, 2))],
+    [(Path(__file__).parent / "data" / "board4.txt", 9, 9, Player.BLACK, Point(1, 2))],
 )
 def test_is_valid_move(game_state, filename, num_rows, num_cols, next_player, point):
     assert game_state.is_valid_move(Move.play(point))

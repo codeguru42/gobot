597a827b4bad430e6d5e715f01b4a0d388801578
Code-Apprentice <codeguru42@gmail.com>
2024-11-17 11:55:06 -0700

    DenseLayer

--- src/deep/layer.py
+++ src/deep/layer.py
@@ -1,5 +1,7 @@
 from typing import Self, Optional
 
+import numpy as np
+
 from deep.sigmoid import sigmoid, sigmoid_prime
 
 
@@ -63,3 +65,40 @@ class ActivationLayer(Layer):
     def describe(self):
         print(f"|-- {self.__class__.__name__}")
         print(f"  |-- dimensions: ({self.input_dim},{self.output_dim})")
+
+
+class DenseLayer(Layer):
+    def __init__(self, input_dim: int, output_dim: int):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.weight = np.random.randn(input_dim, output_dim)
+        self.bias = np.random.randn(output_dim, 1)
+        self.params = [self.weight, self.bias]
+        self.delta_w = np.zeros(self.weight.shape)
+        self.delta_b = np.zeros(self.bias.shape)
+
+    def forward(self):
+        data = self.get_foward_input()
+        self.output_data = np.dot(self.weight, data) + self.bias
+
+    def backward(self):
+        data = self.get_foward_input()
+        delta = self.get_backward_input()
+        self.delta_b += delta
+        self.delta_w += np.dot(delta, data.T)
+        self.output_delta = np.dot(self.weight.T, delta)
+
+    def update_params(self, learning_rate):
+        self.weight -= learning_rate * self.delta_w
+        self.bias -= learning_rate * self.delta_b
+        
+    def clear_deltas(self):
+        self.delta_w = np.zeros(self.weight.shape)
+        self.delta_b = np.zeros(self.bias.shape)
+
+    def describe(self):
+        print(f"|-- {self.__class__.__name__}")
+        print(f"  |-- dimensions: ({self.input_dim},{self.output_dim})")
+        
+        

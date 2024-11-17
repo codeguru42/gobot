84494e78f80cb969b8cf0ac22f75081a2407970e
Code-Apprentice <codeguru42@gmail.com>
2024-11-17 11:37:14 -0700

    predict()

--- src/deep/sigmoid.py
+++ src/deep/sigmoid.py
@@ -7,3 +7,7 @@ def sigmoid_double(x: float) -> float:
 
 def sigmoid(z: np.array) -> np.array:
     return np.vectorize(sigmoid_double)(z)
+
+
+def predict(x: np.array, W: np.array, b: np.array) -> np.array:
+    return sigmoid_double(np.dot(x, W) + b)

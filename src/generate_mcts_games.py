94cdb8e30c5a69c4bb73ab50ee8c3d5e30689186
Code-Apprentice <codeguru42@gmail.com>
2025-01-03 21:50:14 -0700

    Generate some game data

--- src/generate_mcts_games.py
+++ src/generate_mcts_games.py
@@ -1,4 +1,5 @@
 import numpy as np
+import typer
 
 from agents.mcts_agent import MCTSAgent
 from bot_v_bot import print_board, print_move
@@ -6,9 +7,7 @@ from encoders.base import get_encoder_by_name
 from go.goboard import GameState
 
 
-def generate_game(
-    board_size: tuple[int, int], rounds: int, max_moves: int, temperature: float
-):
+def generate_game(board_size: int, rounds: int, max_moves: int, temperature: float):
     boards, moves = [], []
     encoder = get_encoder_by_name("oneplane", board_size)
     game = GameState.new_game(board_size)
@@ -29,3 +28,31 @@ def generate_game(
         if num_moves >= max_moves:
             break
     return np.array(boards), np.array(moves)
+
+
+def main(
+    board_out: str,
+    move_out: str,
+    board_size: int = 9,
+    rounds: int = 1000,
+    temperature: float = 0.8,
+    max_moves: int = 60,
+    num_games: int = 10,
+):
+    xs = []
+    ys = []
+
+    for i in range(num_games):
+        print(f"Generating game {i+1}/{num_games}")
+        x, y = generate_game(board_size, rounds, max_moves, temperature)
+        xs.append(x)
+        ys.append(y)
+
+    x = np.concatenate(xs)
+    y = np.concatenate(ys)
+    np.save(board_out, x)
+    np.save(move_out, y)
+
+
+if __name__ == "__main__":
+    typer.run(main)

8f0dfa895f9afe8d9557783edcc1f1d59918c4fb
Code-Apprentice <codeguru42@gmail.com>
2025-02-23 11:40:40 -0700

    Parse all links in the page

--- src/download_kgs.py
+++ src/download_kgs.py
@@ -1,12 +1,15 @@
 import httpx
 import typer
+from bs4 import BeautifulSoup
 
 url = "https://www.u-go.net/gamerecords/"
 
 def main():
     response = httpx.get(url)
-    typer.echo(response.status_code)
-    typer.echo(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.findAll("a")
+    for link in links:
+        typer.echo(link.get("href"))
 
 if __name__ == '__main__':
     typer.run(main)

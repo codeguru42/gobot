6c5e3fa2dcc1c6db7ab2de7f421667ee8301c17b
Code-Apprentice <codeguru42@gmail.com>
2025-02-23 11:42:55 -0700

    Refactor as a generator

--- src/download_kgs.py
+++ src/download_kgs.py
@@ -2,14 +2,22 @@ import httpx
 import typer
 from bs4 import BeautifulSoup
 
-url = "https://www.u-go.net/gamerecords/"
+kgs_url = "https://www.u-go.net/gamerecords/"
 
 def main():
+    for link in tar_links(kgs_url):
+        typer.echo(link)
+
+
+def tar_links(url: str):
     response = httpx.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    links = soup.findAll("a")
+    links = soup.find_all("a")
     for link in links:
-        typer.echo(link.get("href"))
+        href = link.get("href")
+        if href.endswith(".tar.gz"):
+            yield href
+
 
 if __name__ == '__main__':
     typer.run(main)

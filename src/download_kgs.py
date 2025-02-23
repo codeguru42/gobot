bffb7ac340eee9e46c9c9b8648bc2987dc6d74b5
Code-Apprentice <codeguru42@gmail.com>
2025-02-23 11:37:34 -0700

    Get page with links to SGF records

--- src/download_kgs.py
+++ src/download_kgs.py
@@ -1,7 +1,12 @@
+import httpx
 import typer
 
+url = "https://www.u-go.net/gamerecords/"
+
 def main():
-    ...
+    response = httpx.get(url)
+    typer.echo(response.status_code)
+    typer.echo(response.text)
 
 if __name__ == '__main__':
     typer.run(main)

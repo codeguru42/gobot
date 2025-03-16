6a46cfc219169c7928452dad8270d5c35d1c69d4
Code-Apprentice <codeguru42@gmail.com>
2025-03-15 23:07:58 -0600

    Tokenizer for single character tokens

---
+++ src/sgf/tokenizer.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class TokenType(Enum):
+    L_PAREN = "("
+    R_PAREN = ")"
+    L_BRACKET = "["
+    R_BRACKET = "]"
+    SEMI = ";"
+    COLON = ":"
+    NONE = "NONE"
+    NUMBER = "NUMBER"
+    REAL = "REAL"
+    DOUBLE = "DOUBLE"
+    COLOR = "COLOR"
+    SIMPLE_TEXT = "SIMPLE_TEXT"
+    TEXT = "TEXT"
+    POINT = "POINT"
+    MOVE = "MOVE"
+    STONE = "STONE"
+
+
+@dataclass
+class Token:
+    type: TokenType
+    token: str
+
+
+def tokens(input_stream):
+    for c in input_stream:
+        match c:
+            case "(":
+                yield Token(TokenType.L_PAREN, c)
+            case ")":
+                yield Token(TokenType.R_PAREN, c)
+            case "[":
+                yield Token(TokenType.L_BRACKET, c)
+            case "]":
+                yield Token(TokenType.R_BRACKET, c)
+            case ";":
+                yield Token(TokenType.SEMI, c)
+            case ":":
+                yield Token(TokenType.COLON, c)

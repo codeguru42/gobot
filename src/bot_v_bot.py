8811548b20d960912f0daf795fa23b1a6964e437
Code-Apprentice <codeguru42@gmail.com>
2025-03-30 20:23:57 -0600

    Refactor functions to print board to separate file

--- src/bot_v_bot.py
+++ src/bot_v_bot.py
@@ -1,34 +1,16 @@
+from typing import Optional
+
 from agents.mcts_agent import MCTSAgent
-from go.goboard import Move, Board, GameState
-from go.gotypes import Player, Point
+from go.goboard import GameState
+from go.gotypes import Player
+from utils.print import print_board, print_move
 
 COLS = "ABCDEFGHJKLMNOPQRST"
-STONE_TO_CHAR: dict[Player, str] = {
+STONE_TO_CHAR: dict[Optional[Player], str] = {
     None: ".",
     Player.BLACK: "x",
     Player.WHITE: "o",
 }
-
-
-def print_move(player: Player, move: Move):
-    if move.is_pass:
-        move_str = "passes"
-    elif move.is_resign:
-        move_str = "resigns"
-    else:
-        move_str = f"{COLS[move.point.col - 1]}{move.point.row}"
-    print(player, move_str)
-
-
-def print_board(board: Board):
-    for row in range(board.num_rows, 0, -1):
-        bump = " " if row <= 9 else ""
-        line = []
-        for col in range(1, board.num_cols + 1):
-            stone = board.get(Point(row, col))
-            line.append(STONE_TO_CHAR[stone])
-        print(f"{bump}{row} {"".join(line)}")
-    print("   " + COLS[: board.num_cols])
 
 
 def main():
---
+++ src/utils/print.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from go.goboard import Board, Move
+from go.gotypes import Player, Point
+
+COLS = "ABCDEFGHJKLMNOPQRST"
+STONE_TO_CHAR: dict[Optional[Player], str] = {
+    None: ".",
+    Player.BLACK: "x",
+    Player.WHITE: "o",
+}
+
+
+def print_move(player: Player, move: Move):
+    if move.is_pass:
+        move_str = "passes"
+    elif move.is_resign:
+        move_str = "resigns"
+    else:
+        move_str = f"{COLS[move.point.col - 1]}{move.point.row}"
+    print(player, move_str)
+
+
+def print_board(board: Board):
+    for row in range(board.num_rows, 0, -1):
+        bump = " " if row <= 9 else ""
+        line = []
+        for col in range(1, board.num_cols + 1):
+            stone = board.get(Point(row, col))
+            line.append(STONE_TO_CHAR[stone])
+        print(f"{bump}{row} {"".join(line)}")
+    print("   " + COLS[: board.num_cols])

ed2981433a5ffd01fc72380f3256abdcfef0a9e1
Code-Apprentice <codeguru42@gmail.com>
2025-04-01 16:11:22 -0600

    List files in input directory

--- src/encode.py
+++ src/encode.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 import typer
 
-def main():
-    print("Hello, World!")
+def read_files(input_directory: Path):
+    for file in input_directory.glob("*.tar.gz"):
+        print(file.name)
+
+
+def main(input_directory: Path, output_directory: Path):
+    read_files(input_directory)
 
 
 if __name__ == "__main__":

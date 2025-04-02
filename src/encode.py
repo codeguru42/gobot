b28aedd50b24e14bc6e38a296c307935471135a8
Code-Apprentice <codeguru42@gmail.com>
2025-04-02 00:39:25 -0600

    Parse all files

--- src/encode.py
+++ src/encode.py
@@ -1,10 +1,15 @@
 import tarfile
 from pathlib import Path
+from typing import Iterable
 
 import typer
 
+from parse_sgf import visualize_collection
+from sgf.parser import parse_sgf, Collection
+from sgf.tokenizer import tokens
 
-def extract_files(input_directory: Path):
+
+def extract_files(input_directory: Path) -> Iterable[BufferedReader]:
     for file in input_directory.glob("*.tar.gz"):
         with tarfile.open(file, "r:gz") as tar:
             for member in tar.getmembers():
@@ -12,10 +17,15 @@ def extract_files(input_directory: Path):
                     yield tar.extractfile(member)
 
 
-def main(input_directory: Path, output_directory: Path):
+def parse_files(input_directory: Path) -> Iterable[Collection]:
     for f in extract_files(input_directory):
-        print(f.read())
+        content = f.read().decode("utf-8")
+        yield parse_sgf(tokens(content))
 
+
+def main(input_directory: Path, output_directory: Path):
+    for collection in parse_files(input_directory):
+        visualize_collection(collection)
 
 if __name__ == "__main__":
     typer.run(main)

7e6e9bc2312ab9c6fc4aa9d9023e39405f79629c
Code-Apprentice <codeguru42@gmail.com>
2025-04-02 19:10:53 -0600

    Encode moves with oneplane

--- src/encode.py
+++ src/encode.py
@@ -3,13 +3,14 @@ from io import BufferedReader
 from pathlib import Path
 from typing import Iterable
 
+import numpy as np
 import typer
 
+from encoders.base import get_encoder_by_name
 from go.goboard import GameState
 from replay import visit_collection
 from sgf.parser import parse_sgf, Collection
 from sgf.tokenizer import tokens
-from utils.print import print_move, print_board
 
 
 def extract_files(input_directory: Path) -> Iterable[BufferedReader]:
@@ -22,6 +23,7 @@ def extract_files(input_directory: Path) -> Iterable[BufferedReader]:
 
 def parse_files(sgf_files: Iterable[BufferedReader]) -> Iterable[Collection]:
     for f in sgf_files:
+        typer.echo(f"Parsing {f.name}")
         content = f.read().decode("utf-8")
         yield parse_sgf(tokens(content))
 
@@ -33,17 +35,34 @@ def replay_games(
         yield visit_collection(collection)
 
 
+def encode_game(games) -> Iterable[tuple[np.ndarray, np.ndarray]]:
+    for game in games:
+        for game_state in game:
+            encoder = get_encoder_by_name(
+                "oneplane", (game_state.board.num_cols, game_state.board.num_rows)
+            )
+            if game_state.last_move is not None and game_state.last_move.is_play:
+                typer.echo(f"Last move: {game_state.last_move}")
+                yield (
+                    encoder.encode(game_state),
+                    encoder.encode_point(game_state.last_move.point),
+                )
+
+
+def encode_all(
+    collections: Iterable[Collection],
+) -> Iterable[Iterable[tuple[np.ndarray, np.ndarray]]]:
+    for games in replay_games(collections):
+        yield encode_game(games)
+
+
 def main(input_directory: Path, output_directory: Path):
     sgf_files = extract_files(input_directory)
     collections = parse_files(sgf_files)
-    for games in replay_games(collections):
-        for game in games:
-            for game_state in game:
-                if game_state.last_move:
-                    print_move(
-                        game_state.previous_state.next_player, game_state.last_move
-                    )
-                print_board(game_state.board)
+    for encs in encode_all(collections):
+        for x, y in encs:
+            print(x)
+            print(y)
 
 
 if __name__ == "__main__":

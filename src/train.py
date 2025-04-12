2cdd29b4e20215430c1408264bb3864ef9c264da
Code-Apprentice <codeguru42@gmail.com>
2025-04-11 19:40:49 -0600

    Double quoted strings

--- src/train.py
+++ src/train.py
@@ -81,25 +81,25 @@ def train(
     model = keras.Sequential(
         [
             keras.layers.Input(input_shape),
-            keras.layers.ZeroPadding2D(padding=3, data_format='channels_first'),
-            keras.layers.Conv2D(48, (7, 7), data_format='channels_first'),
+            keras.layers.ZeroPadding2D(padding=3, data_format="channels_first"),
+            keras.layers.Conv2D(48, (7, 7), data_format="channels_first"),
             keras.layers.Activation('relu'),
 
-            keras.layers.ZeroPadding2D(padding=2, data_format='channels_first'),
-            keras.layers.Conv2D(32, (5, 5), data_format='channels_first'),
-            keras.layers.Activation('relu'),
+            keras.layers.ZeroPadding2D(padding=2, data_format="channels_first"),
+            keras.layers.Conv2D(32, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
 
-            keras.layers.ZeroPadding2D(padding=2, data_format='channels_first'),
-            keras.layers.Conv2D(32, (5, 5), data_format='channels_first'),
-            keras.layers.Activation('relu'),
+            keras.layers.ZeroPadding2D(padding=2, data_format="channels_first"),
+            keras.layers.Conv2D(32, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
 
-            keras.layers.ZeroPadding2D(padding=2, data_format='channels_first'),
-            keras.layers.Conv2D(32, (5, 5), data_format='channels_first'),
-            keras.layers.Activation('relu'),
+            keras.layers.ZeroPadding2D(padding=2, data_format="channels_first"),
+            keras.layers.Conv2D(32, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
 
             keras.layers.Flatten(),
             keras.layers.Dense(361),
-            keras.layers.Activation('relu'),
+            keras.layers.Activation("relu"),
         ]
     )
 

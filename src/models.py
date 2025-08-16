7039e3bae650084d38c40f72e49e82eef1ee26e0
Code-Apprentice <codeguru42@gmail.com>
2025-08-16 13:39:37 -0600

    large model

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 import keras
 
 
-def get_sequential_model(input_shape: tuple[int, int, int]):
+def get_small_model(input_shape: tuple[int, int, int]):
     return keras.Sequential(
         [
             keras.layers.Input(input_shape),
@@ -19,6 +19,42 @@ def get_sequential_model(input_shape: tuple[int, int, int]):
             keras.layers.Activation("relu"),
             keras.layers.Flatten(),
             keras.layers.Dense(361),
+            keras.layers.Activation("relu"),
+        ]
+    )
+
+
+def get_large_model(input_shape: tuple[int, int, int]):
+    return keras.Sequential(
+        [
+            keras.layers.Input(input_shape),
+            keras.layers.ZeroPadding2D(
+                (3, 3), input_shape=input_shape, data_format="channels_first"
+            ),
+            keras.layers.Conv2D(
+                64, (7, 7), padding="valid", data_format="channels_first"
+            ),
+            keras.layers.Activation("relu"),
+            keras.layers.ZeroPadding2D((2, 2), data_format="channels_first"),
+            keras.layers.Conv2D(64, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
+            keras.layers.ZeroPadding2D((2, 2), data_format="channels_first"),
+            keras.layers.Conv2D(64, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
+            keras.layers.ZeroPadding2D((2, 2), data_format="channels_first"),
+            keras.layers.Conv2D(48, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
+            keras.layers.ZeroPadding2D((2, 2), data_format="channels_first"),
+            keras.layers.Conv2D(48, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
+            keras.layers.ZeroPadding2D((2, 2), data_format="channels_first"),
+            keras.layers.Conv2D(32, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
+            keras.layers.ZeroPadding2D((2, 2), data_format="channels_first"),
+            keras.layers.Conv2D(32, (5, 5), data_format="channels_first"),
+            keras.layers.Activation("relu"),
+            keras.layers.Flatten(),
+            keras.layers.Dense(1024),
             keras.layers.Activation("relu"),
         ]
     )

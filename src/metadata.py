112d732b19765e5b96f1fb25aa190bd89f4bfedc
Code-Apprentice <codeguru42@gmail.com>
2025-09-13 17:29:29 -0600

    decode_metadata() helper function

--- src/metadata.py
+++ src/metadata.py
@@ -12,7 +12,22 @@ class TarfileMetadata:
     path: str
     game_count: int
     games: list[GameMetadata]
-    
+
     @property
     def move_count(self):
         return sum(game.move_count for game in self.games)
+
+
+def decode_metadata(data):
+    return TarfileMetadata(
+        path=data["path"],
+        game_count=data["game_count"],
+        games=list(
+            decode_games(data["games"]),
+        ),
+    )
+
+
+def decode_games(data):
+    for game in data:
+        yield GameMetadata(name=data["name"], move_count=game["move_count"])
